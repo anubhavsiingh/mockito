6ce33ce1f05ebcc0e0a0249ad11c065818464b6e
Anubhav Singh <shwyamr@gmail.com>
2021-11-30 15:07:43 +0530

    Initial Commit

---
+++ ShopStop/src/main/java/com/coforge/training/shopstop/Login.java
@@ -0,0 +1,47 @@
+package com.coforge.training.shopstop;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private String uname;
+	private String pass;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		uname = request.getParameter("userName");
+		pass = request.getParameter("userPassword");
+		
+		Date d1 = new Date();
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println(uname + pass);
+		out.println("Login time" + d1); 
+	}
+
+}
